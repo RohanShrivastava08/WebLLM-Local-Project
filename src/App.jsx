41f99f90c3a598b7bda020cee6245e07ad94b667
RohanShrivastava08 <rohansh0808@gmail.com>
2025-04-22 17:16:37 +0530

    Added app.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
+import './app.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    Hello
+    <main>
+      <section>
+        <div className="conversation-area">
+
+          <div className="input-area">
+            <input type="text" placeholder='Message LLM' />
+            <button>Send</button>
+          </div>
+        </div>
+      </section>
+    </main>
     </>
   )
 }
